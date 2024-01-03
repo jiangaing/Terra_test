ce5ef083115ab8dfa9cf18bf4927695625aee992
Chang <j.chang@erasmusmc.nl>
2024-01-03 17:08:39 +0000

    update

--- scripts/test_ cerebroViz.R
+++
@@ -1,8 +0,0 @@
-library(devtools)
-library("cerebroViz")
-####install_github("ethanbahl/cerebroViz")
-
-vignette(topic="intro_cerebroViz", package="cerebroViz")
-data(cerebroEx)
-head(cerebroEx)[, c(1:7)]
-cerebroViz(cerebroEx)
